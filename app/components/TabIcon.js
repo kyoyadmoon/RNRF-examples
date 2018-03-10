1ad85ce94a3eb05a049b95869c1a0ec5287e5260
dmoon <dmoon.t@gmail.com>
2018-03-11 00:35:03 +0800

    Update TabIcon

--- app/components/TabIcon.js
+++ app/components/TabIcon.js
@@ -3,14 +3,13 @@ import PropTypes from "prop-types";
 import { Text } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 
-const RocketIcon = <Icon name="rocket" size={20} />;
-
 const propTypes = {
   selected: PropTypes.bool,
   title: PropTypes.string
 };
 
 const TabIcon = props => {
+  //  <Icon name="rocket" size={20} />;
   return (
     <Text style={{ color: props.focused ? "red" : "black" }}>
       {props.title}
